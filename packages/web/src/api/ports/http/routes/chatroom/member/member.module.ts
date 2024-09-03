acc63649afa717724c42fbbc4843d0ae109d117a
i-play-lf2 <914637118@qq.com>
2024-09-03 10:02:41 +0000

    wip

--- packages/web/src/api/ports/http/routes/chatroom/member/member.module.ts
+++ packages/web/src/api/ports/http/routes/chatroom/member/member.module.ts
@@ -1,10 +1,11 @@
 import { AuthModule } from '../../../auth/auth.module.js'
 import { MemberController } from './member.controller.js'
 import { Module } from '@nestjs/common'
+import { conversation } from '../../../../../../domains/conversation/group/group.module.js'
 
 @Module({
   controllers: [MemberController],
-  imports: [AuthModule],
+  imports: [AuthModule, conversation.GroupModule],
 })
 export class MemberModule {
 }
