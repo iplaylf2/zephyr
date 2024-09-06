bbb40e315f3324c310900c8c4dd09f7dc1a075e7
i-play-lf2 <914637118@qq.com>
2024-09-06 07:36:41 +0000

    wip

--- packages/web/src/api/ports/http/kits/router.ts
+++ packages/web/src/api/ports/http/kits/router.ts
@@ -1,29 +1,28 @@
 import { DynamicModule, Type } from '@nestjs/common'
-import { RouterModule, Routes } from '@nestjs/core'
+import { RouteTree, RouterModule, Routes } from '@nestjs/core'
 import { either, readonlyArray } from 'fp-ts'
-import { flow, pipe } from 'fp-ts/lib/function.js'
+import { pipe } from 'fp-ts/lib/function.js'
 
 export namespace router{
   export function register(routes: Routes): (DynamicModule | Type)[] {
     const modules = pipe(
       routes,
-      readonlyArray.chainRecBreadthFirst(
-        readonlyArray.match(
-          () => [],
-          readonlyArray.chain(flow(
-            ({ children, module }) => [
-              pipe(
-                children ?? [],
-                readonlyArray.partition(x => 'function' === typeof x),
-                ({ left: routes, right: modules }) => [
-                  either.left(routes as Routes),
-                  ...modules.map(either.right),
-                ],
+      readonlyArray.chain(
+        readonlyArray.chainRecBreadthFirst(
+          ({ children, module }) => [
+            pipe(
+              children ?? [],
+              readonlyArray.map(
+                either.fromPredicate(
+                  x => 'function' === typeof x,
+                  x => x as RouteTree,
+                ),
               ),
-              module ? [either.right(module)] : [],
-            ],
-            x => x.flat(),
-          )))),
+            ),
+            module ? [either.right(module)] : [],
+          ].flat(),
+        ),
+      ),
     )
 
     return [...modules, RouterModule.register(routes)]
