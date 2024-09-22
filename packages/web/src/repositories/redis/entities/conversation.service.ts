57e2d347b38f21cf750b31330ac596b8786d17e4
i-play-lf2 <914637118@qq.com>
2024-09-22 07:30:41 +0000

    wip

--- packages/web/src/repositories/redis/entities/conversation.service.ts
+++ packages/web/src/repositories/redis/entities/conversation.service.ts
@@ -1,10 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common'
-import { JsonHash } from './common/json-hash.js'
 import { JsonStream } from './common/json-stream.js'
 import { RedisClientType } from '@redis/client'
 import { RedisService } from '../redis.service.js'
-import { Set } from '../commands/set.js'
-import { SortedSet } from '../commands/sorted-set.js'
 import { conversation } from '../../../models/conversation.js'
 
 @Injectable()
@@ -12,38 +9,8 @@ export class ConversationService {
   @Inject()
   private readonly redisService!: RedisService
 
-  public get(type: string) {
-    return new Conversations(this.redisService, type)
-  }
-
-  public getParticipantConversations(type: string, participant: string) {
-    return new Conversation.Participant.Conversations(this.redisService, type, participant)
-  }
-
-  public getParticipantConversationsMarked(type: string, participant: string) {
-    return new Conversation.Participant.ConversationsMarked(this.redisService, type, participant)
-  }
-
-  public getParticipantConversationsProgress(type: string, participant: string) {
-    return new Conversation.Participant.ConversationsProgress(this.redisService, type, participant)
-  }
-
-  public getParticipants(type: string, conversation: string) {
-    return new Conversations.Participants(this.redisService, type, conversation)
-  }
-
   public getRecords(type: string, conversation: number) {
     return Conversations.Records.get(this.redisService, type, conversation)
-  }
-}
-
-export class Conversations extends SortedSet {
-  public override readonly key
-
-  public constructor(public override client: RedisClientType, type: string) {
-    super()
-
-    this.key = `sorted-set://${encodeURIComponent(type)}.conversations` as const
   }
 }
 
@@ -64,48 +31,6 @@ export namespace Conversations{
 
     protected override duplicate() {
       return new Records(this.client.duplicate(), this.key)
-    }
-  }
-
-  export class Participants extends SortedSet {
-    public override readonly key
-
-    public constructor(public override client: RedisClientType, type: string, id: string) {
-      super()
-
-      this.key = `sorted-set://${encodeURIComponent(type)}.conversations/${encodeURIComponent(id)}/participants` as const
-    }
-  }
-}
-
-export namespace Conversation.Participant{
-  export class Conversations extends Set {
-    public override readonly key
-
-    public constructor(public override client: RedisClientType, type: string, participant: string) {
-      super()
-
-      this.key = `set://${encodeURIComponent(type)}.conversation/participants/${encodeURIComponent(participant)}/conversations` as const
-    }
-  }
-
-  export class ConversationsProgress extends JsonHash<Record<string, string>> {
-    public override readonly key
-
-    public constructor(public override client: RedisClientType, type: string, participant: string) {
-      super()
-
-      this.key = `hash://${encodeURIComponent(type)}.conversation/participants/${encodeURIComponent(participant)}/conversations/progress` as const
-    }
-  }
-
-  export class ConversationsMarked extends JsonHash<Record<string, string>> {
-    public override readonly key
-
-    public constructor(public override client: RedisClientType, type: string, participant: string) {
-      super()
-
-      this.key = `hash://${encodeURIComponent(type)}.conversation/participants/${encodeURIComponent(participant)}/conversations/marked` as const
     }
   }
 }
--- packages/web/src/repositories/redis/entities/user.service.ts
+++ packages/web/src/repositories/redis/entities/user.service.ts
@@ -1,9 +1,7 @@
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
-import { JsonHash } from './common/json-hash.js'
 import { JsonStream } from './common/json-stream.js'
 import { RedisClientType } from '@redis/client'
 import { RedisService } from '../redis.service.js'
-import { SortedSet } from '../commands/sorted-set.js'
 import { call } from 'effection'
 import { globalScope } from '../../../kits/effection/global-scope.js'
 import { user } from '../../../models/user.js'
@@ -12,16 +10,8 @@ import { user } from '../../../models/user.js'
 export class UserService implements OnModuleInit {
   @Inject() private readonly redisService!: RedisService
 
-  public get() {
-    return new Users(this.redisService)
-  }
-
   public getEvent() {
     return new User.Event(this.redisService)
-  }
-
-  public getInfo(id: string) {
-    return new User.Info(this.redisService, id)
   }
 
   public onModuleInit() {
@@ -38,14 +28,6 @@ export class UserService implements OnModuleInit {
   }
 }
 
-export class Users extends SortedSet {
-  public override readonly key = `sorted-set://users`
-
-  public constructor(public override client: RedisClientType) {
-    super()
-  }
-}
-
 export namespace User{
   export class Event extends JsonStream<user.Event> {
     public override readonly key = `stream://user/event`
@@ -56,16 +38,6 @@ export namespace User{
 
     protected override duplicate() {
       return new Event(this.client.duplicate())
-    }
-  }
-
-  export class Info extends JsonHash<user.Info> {
-    public override readonly key
-
-    public constructor(public override client: RedisClientType, id: string) {
-      super()
-
-      this.key = `hash://users/${encodeURIComponent(id)}/info`
     }
   }
 }
