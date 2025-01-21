c88718129f08218e79e4ce030bad622525583b90
i-play-lf2 <914637118@qq.com>
2025-01-21 05:44:02 +0000

    fix merge

--- packages/web/src/kits/effection/merge.ts
+++ packages/web/src/kits/effection/merge.ts
@@ -1,34 +1,32 @@
-import { Operation, Yielded, scoped, spawn, withResolvers } from 'effection'
+import { Operation, Yielded, spawn, withResolvers } from 'effection'
 
 export function* merge<O1 extends Operation<unknown>, O2 extends Operation<unknown>>(
   o1: O1,
   o2: O2): Operation<[Yielded<O1>, O2] | [Yielded<O2>, O1]> {
-  return yield * scoped(function*() {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { operation, reject, resolve } = withResolvers<[Yielded<O1>, O2] | [Yielded<O2>, O1]>()
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { operation, reject, resolve } = withResolvers<[Yielded<O1>, O2] | [Yielded<O2>, O1]>()
 
-    void (yield * spawn(function*() {
-      try {
-        const v1 = yield * o1
+  void (yield * spawn(function*() {
+    try {
+      const v1 = yield * o1
 
-        resolve([v1 as Yielded<O1>, o2])
-      }
-      catch (e) {
-        reject(e as Error)
-      }
-    }))
+      resolve([v1 as Yielded<O1>, o2])
+    }
+    catch (e) {
+      reject(e as Error)
+    }
+  }))
 
-    void (yield * spawn(function*() {
-      try {
-        const v2 = yield * o2
+  void (yield * spawn(function*() {
+    try {
+      const v2 = yield * o2
 
-        resolve([v2 as Yielded<O2>, o1])
-      }
-      catch (e) {
-        reject(e as Error)
-      }
-    }))
+      resolve([v2 as Yielded<O2>, o1])
+    }
+    catch (e) {
+      reject(e as Error)
+    }
+  }))
 
-    return yield * operation
-  })
+  return yield * operation
 }
