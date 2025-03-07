40210c0fa9d89fc8c055eeffc0f4b88ee6d479c2
i-play-lf2 <914637118@qq.com>
2025-03-07 07:02:43 +0000

    improve merge

--- packages/kit/src/effection/merge.ts
+++ packages/kit/src/effection/merge.ts
@@ -3,7 +3,8 @@ import { Operation, useScope, withResolvers } from 'effection'
 
 export function* merge<T1, T2>(
   o1: () => Operation<T1>,
-  o2: () => Operation<T2>): Directive<[T1, Procedure<T2>] | [T2, Procedure<T1>]> {
+  o2: () => Operation<T2>,
+): Directive<[T1, Procedure<T2>] | [T2, Procedure<T1>]> {
   const scope = yield* useScope()
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -12,19 +13,23 @@ export function* merge<T1, T2>(
   const task1 = scope.run(o1)
   const task2 = scope.run(o2)
 
-  void task1.then((v1) => {
-    resolve([v1, task2])
-  })
-  task1.catch((e: unknown) => {
-    reject(e as never)
-  })
+  task1.then(
+    (v1) => {
+      resolve([v1, task2])
+    },
+    (e: unknown) => {
+      reject(e as never)
+    },
+  )
 
-  void task2.then((v2) => {
-    resolve([v2, task1])
-  })
-  task2.catch((e: unknown) => {
-    reject(e as never)
-  })
+  task2.then(
+    (v2) => {
+      resolve([v2, task1])
+    },
+    (e: unknown) => {
+      reject(e as never)
+    },
+  )
 
   return yield* operation
 }
